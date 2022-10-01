1c33b8063ee1234c7c402a7619f1653a5b6cbfa5
Ki Hong Park <kihong.park@Kis-MacBook-Pro.local>
2022-10-01 02:11:57 -0500

    Adds master header

---
+++ project_2/master.h
@@ -0,0 +1,32 @@
+/*
+ * master.h
+ *
+ *  Created on: Sep 29, 2022
+ *      Author: Daniel Park
+ */
+
+#ifndef PROJECT_2_MASTER_H_
+#define PROJECT_2_MASTER_H_
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <time.h>
+
+#define MAX_NUM_SLAVE 20
+#define SHMKEY 859047
+#define BUFF_SZ	sizeof ( int )
+
+void help();
+void callSlaves(int numOfSlaves);
+void timeout();
+void interrupt();
+void terminate();
+
+#endif /* PROJECT_2_MASTER_H_ */
