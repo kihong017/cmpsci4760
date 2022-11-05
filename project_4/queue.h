a38f643f972d70d1155d1ab7880296ee5016f90c
Ki Hong Park <kihong.park@Kis-MacBook-Pro.local>
2022-11-05 10:48:58 -0500

    Project 4: Adds isTerminal to extract list

--- project_4/queue.h
+++ project_4/queue.h
@@ -22,6 +22,7 @@ struct Queue {
 
 struct Queue* createQueue(unsigned capacity);
 int isFull(struct Queue* queue);
+int isEmpty(struct Queue* queue);
 void enqueue(struct Queue* queue, int item);
 int dequeue(struct Queue* queue);
 int front(struct Queue* queue);
